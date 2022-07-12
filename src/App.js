7acefa7b7575e20318d83b34bf990c771629388f
Aaron Ivan Amaro <aaronivanamaro@gmail.com>
2022-07-11 21:25:09 -0300

    useEffect ejemplo con llamadas a apis

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import { useState } from 'react';
 import './App.css';
 import Lista from './components/Lista';
 
-const listaDeSeries = [
-  { id: 1, nombre: 'Stranger Things', label: 'Netflix' },
-  { id: 2, nombre: 'Game of Thrones', label: 'HBO' },
-  { id: 3, nombre: 'The last kingdom', label: 'Netflix' },
-  { id: 4, nombre: 'Supernatural', label: 'Amazon' },
-  { id: 5, nombre: 'Star Trek', label: 'Netflix' },
-  { id: 6, nombre: 'Alf', label: 'Amazon' },
-  { id: 7, nombre: 'Orphan Black', label: 'Netflix' },
-];
-
 function App() {
-
-  const [series, setSeries] = useState(listaDeSeries);
-
-  function eliminarItem(itemAEliminar){
-    const seriesFiltradas = series.filter(serie => serie.id !== itemAEliminar);
-    setSeries(seriesFiltradas);
-  }
 
   return (
     <div>
-
-      <Lista contenido={series} tituloDeLista='Series' eliminar={eliminarItem}>
-        Esta es una lista de series y las plataformas donde podemos encontrarlas
-      </Lista>
+      <Lista />
 
     </div>
   );
--- src/components/Lista.js
+++ src/components/Lista.js
@@ -1,21 +1,94 @@
-import Card from "./Card";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const Lista = (props) => {
+const Lista = () => {
+    const [botonActualizador, setBotonActualizador] = useState(false);
+
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState({
+        isError: false,
+        message: '',
+    });
+
+    const [posts, setPosts] = useState([]);
+    const [isPostsLoading, setIsPostsLoading] = useState(false);
+    const [postsError, setPostsError] = useState({
+        isError: false,
+        message: '',
+    });
+
+    const handleClick = () => setBotonActualizador(!botonActualizador);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const res = await axios('https://jsonplaceholder.typicode.com/users');
+                setData(res.data);
+            } catch (err) {
+                setError({
+                    isError: true,
+                    message: err.message
+                })
+            } finally {
+                setTimeout(() => {
+                    setIsLoading(false);
+                }, 1000)
+            }
+        }
+        fetchData();
+    }, [botonActualizador]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsPostsLoading(true);
+            try {
+                const res = await axios('http://localhost:8000/team');
+                setPosts(res.data);
+            } catch (err) {
+                setPostsError({
+                    isError: true,
+                    message: err.message
+                })
+            } finally {
+                setTimeout(() => {
+                    setIsPostsLoading(false);
+                }, 1000)
+            }
+        }
+        fetchData();
+    }, [botonActualizador]);
 
     return (
         <>
-            <h2>Lista de {props.tituloDeLista}</h2>
-            <p>{ props.children }</p>
+            <button onClick={handleClick}>Botón Actualizador</button>
+
             <ul>
-                {props.contenido.map((serie, index) => (
-                    <Card 
-                        key={serie.id}
-                        nombre={serie.nombre}
-                        label={serie.label}
-                        id={serie.id}
-                        eliminar={props.eliminar}
-                    />
-                ))}
+                {error.isError ? <h4>Error: {error.message}</h4> :
+
+                    isLoading ? <h4>Cargando...</h4> :
+
+                        data.length !== 0 &&
+
+                        data.map(item => (
+                            <li key={item.id}>{item.name} --- {item.email}</li>
+                        ))
+
+                }
+            </ul>
+
+            <ul>
+                {postsError.isError ? <h4>Error: {postsError.message}</h4> :
+
+                    isPostsLoading ? <h4>Cargando...</h4> :
+
+                        posts.length !== 0 &&
+
+                        posts.matches.map(item => (
+                            <li key={item.id}>{item.rival}</li>
+                        ))
+                }
             </ul>
         </>
     )
