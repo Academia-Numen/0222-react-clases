7707cc1cad9716e0f4ccba34678907824773ff50
Aaron Ivan Amaro <aaronivanamaro@gmail.com>
2022-08-01 19:59:57 -0300

    clase 7: ejemplo useReducer

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,12 +1,18 @@
+import { useState } from "react";
+import { useReducer } from "react"
+import counterReducer, { ACTIONS } from "../../reducers/counterReducer";
 
 export default function Counter() {
+    const [count, dispatch] = useReducer(counterReducer, 0);
+    const [value, setValue] = useState('');
 
     return(
         <div>
-            <span>Contador</span>
-            <button>+</button>
-            <button>-</button>
-            <button>Reset</button>
+            <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+            <span>Contador: {count}</span>
+            <button onClick={() => dispatch({ type: ACTIONS.INCREMENT, payload: value })}>+</button>
+            <button onClick={() => dispatch({ type: ACTIONS.DECREMENT, payload: value })}>-</button>
+            <button onClick={() => dispatch({ type: ACTIONS.RESET })}>Reset</button>
         </div>
     )
 }
